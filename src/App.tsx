f772c567f8159dccc99b0267a32bebe3a5c52495
Jean Sfeir <43392082+EuBeans@users.noreply.github.com>
2022-01-16 16:12:48 -0500

    final product for challenge

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Card from './components/Card';
-
-var DatePicker = require("react-bootstrap-date-picker");
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 function App() {
   //json object that stores api fetch data
-  const [nasaData, setNasaData] = React.useState({
+  const [nasaData, setNasaData] = React.useState([{
     copyright: '',
     date: '',
     explanation: '',
@@ -18,36 +18,96 @@ function App() {
     service_version: '',
     title: '',
     url: ''
-  });
+  }]);
 
-  const [date, setDate] = React.useState(new Date());
-
-  const handleChangeDate = (date: Date) => {
-    setDate(date);
-  }
+  const [startDate, setStartDate] = React.useState(new Date(Date.now()));
+  const [endDate, setEndDate] = React.useState(new Date(Date.now()));
 
 
-  useEffect(() => {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=GJeb9jLlI7sPvboK4n6Rn21TxAehRMLAkatmhdBF&start_date=' + '2017-07-08' +'&end_date=' + '2017-07-10')
+  //store in process.env 
+  process.env['REACT_APP_NASA_API_KEY'] = 'GJeb9jLlI7sPvboK4n6Rn21TxAehRMLAkatmhdBF';
+  
+  const onStartDateChange = (date: Date) => {
+    if (date) {
+      setStartDate(date);
+    }
+  };
+
+  const onEndDateChange = (date: Date) => {
+    if (date) {
+      setEndDate(date);
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=GJeb9jLlI7sPvboK4n6Rn21TxAehRMLAkatmhdBF&start_date=${startDate.toISOString().split('T')[0]}&end_date=${endDate.toISOString().split('T')[0]}`)
       .then(res => res.json())
       .then(data => {
-        setNasaData(data); 
-      });
-  }, [])
+        setNasaData(data);
+      })
+      .catch(err => console.log(err));
+  }
+
     
   return (
     <div className="App" style={{backgroundColor:'#92A9BD'}}>
        <div className="container" style={{padding:50}}>
-        <div className="row">
-          <div className="col-md-12">
-            <DatePicker id="example-datepicker" value={date} onChange={handleChangeDate} />
+         <div className="row ">
+          <h1 style={{textAlign:'center'}}>NASA Astronomy Picture of the Day</h1>
+            <form className="form-inline"  onSubmit={onSubmit}>
+              <div className="row">
+                <div className="form-group" style={{width:200}}>
+                  <label>Start Date:</label>
+                  <DatePicker
+                    
+                    selected={startDate}
+                    onChange={onStartDateChange}
+                    dateFormat="yyyy-MM-dd"
+                    className="form-control"
+                    placeholderText="Start Date"
+                    maxDate={endDate}
+                  />
+                </div>
+                <div className="form-group" style={{width:200}}>
+                  <label>End Date:</label>
+                  <DatePicker
+
+                    selected={endDate}
+                    onChange={onEndDateChange}
+                    dateFormat="yyyy-MM-dd"
+                    className="form-control"
+                    placeholderText="End Date"
+                    minDate={startDate}
+                  />
+                </div>
+                <div className="d-flex align-items-end" style={{width:100, }}>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+                  
+              </div>
+              
+            </form>
+
           </div>
-        </div>
-        <div className="row" >
-          <div className="col-md-5">
-            <Card nasaData={nasaData} ></Card>
+
+          {nasaData.map( (data, index) => {
+             
+             return (
+              <div key={index} className="row" style={{marginTop:50}} >
+                <div className="col-md-5">
+                  {data.url && 
+                  <Card
+                    nasaData={data}
+                  />
+                  }
+                </div>
             </div>
-          </div>
+               
+
+             )}
+           )}
+
 
       </div>
    
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,14 +21,11 @@ export default function Card(props: Props) {
 
     //like button state 
     const [like, setLike] = React.useState(false);
-    const [cliked, setClicked] = React.useState(false);
-    //like button click handler 
+
     // set in local storage
     const handleLike = () => {
         setLike(!like);
-        localStorage.setItem('like', JSON.stringify(!like));
-        //if like is now On change button to heart
-        console.log(like)
+        localStorage.setItem(props.nasaData.date, JSON.stringify(!like));
     }
 
     useEffect(() => {
@@ -36,7 +33,7 @@ export default function Card(props: Props) {
     }, [])
 
    const getInitialLikeState = () => {
-        const likeState = localStorage.getItem('like');
+        const likeState = localStorage.getItem(props.nasaData.date);
         if (likeState === 'true') {
             setLike(true);
         } else {
@@ -49,25 +46,28 @@ export default function Card(props: Props) {
     }, [props.nasaData])
 
     return (
-        <div className="card">
-            <div className="card-img-top">
-                <img className="img" src={props.nasaData.url} alt={props.nasaData.title} />
-            </div>
-            <div className="card-body">
-                <div className="card-content">
-                    <h3>{props.nasaData.title}  {props.nasaData.date}</h3>
-                    <p>{props.nasaData.explanation}</p>
-                    
-                        <button
-                            onClick={handleLike}
-                            className={`btn  ${like ? 'btn-danger' : 'btn-secondary'}`}
-                            style={{width: '90px'}}
+             
+            <div className="card">
+                <div className="card-img-top">
+                    <img className="img" src={props.nasaData.url} alt={props.nasaData.title} />
+                </div>
+                <div className="card-body">
+                    <div className="card-content">
+                        <h3>{props.nasaData.title}  {props.nasaData.date}</h3>
+                        <p>{props.nasaData.explanation}</p>
+                        
+                            <button
+                                onClick={handleLike}
+                                className={`btn  ${like ? 'btn-danger' : 'btn-secondary'}`}
+                                style={{width: '90px'}}
 
-                        >
-                            {like ? 'Liked ❤️' : 'Like ♡'}
-                        </button>
+                            >
+                                {like ? 'Liked ❤️' : 'Like ♡'}
+                            </button>
+                    </div>
                 </div>
             </div>
-        </div>
+
     );
+
 }
